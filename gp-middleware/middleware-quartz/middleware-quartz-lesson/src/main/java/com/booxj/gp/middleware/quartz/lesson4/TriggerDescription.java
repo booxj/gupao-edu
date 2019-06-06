bf9fbd2c74e5f5ae6528b0eb4b086fd4014dd14d
booxj <1416890997@qq.com>
2019-06-06 11:10:39 +0800

    #add comment

--- gp-middleware/middleware-quartz/middleware-quartz-lesson/src/main/java/com/booxj/gp/middleware/quartz/lesson4/TriggerDescription.java
+++ gp-middleware/middleware-quartz/middleware-quartz-lesson/src/main/java/com/booxj/gp/middleware/quartz/lesson4/TriggerDescription.java
@@ -12,6 +12,19 @@ import org.quartz.impl.calendar.HolidayCalendar;
  * endTime
  * <p>优先级(priority)
  * <p>错过触发(misfire Instructions)
+ * <pre>
+ *     CronTrigger
+ *      - withMisfireHandlingInstructionDoNothing   不触发立即执行;等待下次Cron触发频率到达时刻开始按照Cron频率依次执行
+ *      - withMisfireHandlingInstructionIgnoreMisfires  以错过的第一个频率时间立刻开始执行;重做错过的所有频率周期后;当下一次触发频率发生时间大于当前时间后，再按照正常的Cron频率依次执行
+ *      - withMisfireHandlingInstructionFireAndProceed  以当前时间为触发频率立刻触发一次执行;然后按照Cron频率依次执行
+ *     SimpleTrigger
+ *      - withMisfireHandlingInstructionFireNow 以当前时间为触发频率立即触发执行;执行至FinalTIme的剩余周期次数,以调度或恢复调度的时刻为基准的周期频率，FinalTime根据剩余次数和当前时间计算得到;调整后的FinalTime会略大于根据starttime计算的到的FinalTime值
+ *      - withMisfireHandlingInstructionIgnoreMisfires  以错过的第一个频率时间立刻开始执行;重做错过的所有频率周期;当下一次触发频率发生时间大于当前时间以后，按照Interval的依次执行剩下的频率;共执行RepeatCount+1次
+ *      - withMisfireHandlingInstructionNextWithExistingCount   不触发立即执行;等待下次触发频率周期时刻，执行至FinalTime的剩余周期次数;以startTime为基准计算周期频率，并得到FinalTime;即使中间出现pause，resume以后保持FinalTime时间不变
+ *      - withMisfireHandlingInstructionNowWithExistingCount    以当前时间为触发频率立即触发执行;执行至FinalTIme的剩余周期次数;以调度或恢复调度的时刻为基准的周期频率，FinalTime根据剩余次数和当前时间计算得到;调整后的FinalTime会略大于根据starttime计算的到的FinalTime值
+ *      - withMisfireHandlingInstructionNextWithRemainingCount  不触发立即执行;等待下次触发频率周期时刻，执行至FinalTime的剩余周期次数;以startTime为基准计算周期频率，并得到FinalTime;即使中间出现pause，resume以后保持FinalTime时间不变
+ *      - withMisfireHandlingInstructionNowWithRemainingCount   以当前时间为触发频率立即触发执行;执行至FinalTIme的剩余周期次数;以调度或恢复调度的时刻为基准的周期频率，FinalTime根据剩余次数和当前时间计算得到
+ * </pre>
  * <p>日历示例(calendar)
  *
  * @author booxj
@@ -34,7 +47,7 @@ public class TriggerDescription {
         Trigger t = TriggerBuilder.newTrigger()
                 .withIdentity("myTrigger")
                 .forJob("myJob")
-                .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(9, 30)) // execute job daily at 9:30
+                .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(9, 30).withMisfireHandlingInstructionDoNothing()) // execute job daily at 9:30
                 .modifiedByCalendar("myHolidays") // but not on holidays
                 .build();
 
