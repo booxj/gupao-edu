8ca7b2d91944aad403e37ea0ac97744ded26bf60
booxj <1416890997@qq.com>
2019-06-12 15:07:15 +0800

    #add microservice.pom

--- gp-core/src/main/java/com/booxj/gp/core/utils/spring/PropertiesUtils.java
+++ gp-core/src/main/java/com/booxj/gp/core/utils/spring/PropertiesUtils.java
@@ -1,23 +1,16 @@
 package com.booxj.gp.core.utils.spring;
 
 import com.booxj.gp.core.utils.CollectionUtils;
-import com.booxj.gp.core.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.util.DefaultPropertiesPersister;
-import org.springframework.util.PropertiesPersister;
-import org.springframework.util.SystemPropertyUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
 import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 
@@ -25,8 +18,6 @@ import java.util.Set;
 public class PropertiesUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(PropertiesUtils.class);
-
-    private static final String XML_FILE_EXTENSION = ".xml";
 
     public static Properties findPackageProperties(String scanPackages) throws IOException {
 
@@ -39,7 +30,8 @@ public class PropertiesUtils {
 
         for (String location : packages) {
             for (Resource resource : resolver.getResources(location)) {
-                fillProperties(properties, new EncodedResource(resource, (Charset) null), new DefaultPropertiesPersister());
+                PropertiesLoaderUtils.fillProperties(properties, new EncodedResource(resource, (Charset) null));
+//                PropertiesLoaderUtils.loadProperties(resource);
             }
         }
 
@@ -48,33 +40,6 @@ public class PropertiesUtils {
 
     static Set<String> checkPackage(String scanPackages) {
         return CollectionUtils.String2Set(scanPackages);
-    }
-
-    static void fillProperties(Properties props, EncodedResource resource, PropertiesPersister persister)
-            throws IOException {
-
-        InputStream stream = null;
-        Reader reader = null;
-        try {
-            String filename = resource.getResource().getFilename();
-            if (filename != null && filename.endsWith(XML_FILE_EXTENSION)) {
-                stream = resource.getInputStream();
-                persister.loadFromXml(props, stream);
-            } else if (resource.requiresReader()) {
-                reader = resource.getReader();
-                persister.load(props, reader);
-            } else {
-                stream = resource.getInputStream();
-                persister.load(props, stream);
-            }
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }
-            if (reader != null) {
-                reader.close();
-            }
-        }
     }
 
 }
